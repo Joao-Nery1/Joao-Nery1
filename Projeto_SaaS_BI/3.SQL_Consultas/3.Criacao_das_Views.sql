6bbda40d7abd5ba6810c80ece06e0441e9484f01
João Batista Silva Nery <joaobatistasilvanery@gmail.com>
2025-06-19 10:15:00 -0300

    Update 3.Criacao_das_Views.sql

--- Projeto_SaaS_BI/3.SQL_Consultas/3.Criacao_das_Views.sql
+++ Projeto_SaaS_BI/3.SQL_Consultas/3.Criacao_das_Views.sql
@@ -1,4 +1,3 @@
-
 --Criação das Views
 
 -- VIEW 1: Receita Mensal + Receita por Plano (MRR)
@@ -7,6 +6,7 @@ CREATE VIEW vw_Faturamento_Mensal_Por_Plano AS
 SELECT
     YEAR(CONVERT(DATE, CAST(F_Payment.date_sk AS CHAR(8)), 112)) AS Ano,       -- Ano do pagamento
     MONTH(CONVERT(DATE, CAST(F_Payment.date_sk AS CHAR(8)), 112)) AS Mes,      -- Mês do pagamento
+    DAY(CONVERT(DATE, CAST(F_Payment.date_sk AS CHAR(8)), 112)) AS Dia,      -- Dia do pagamento
     D_Subscription.plan_type,                                 -- Tipo de plano (Ex: Basic, Premium)
     COUNT(DISTINCT F_Payment.customer_sk) AS Total_Clientes_Pagantes,  -- Total de clientes que pagaram no mês
     SUM(F_Payment.amount_paid) AS Receita_Total               -- Soma total das receitas no mês
@@ -14,8 +14,9 @@ FROM F_Payment
 JOIN D_Subscription ON F_Payment.subscription_sk = D_Subscription.subscription_sk  -- Relaciona pagamento com a assinatura
 WHERE F_Payment.payment_status = 'Pago'                      -- Considera apenas pagamentos efetivamente pagos
 GROUP BY   YEAR(CONVERT(DATE, CAST(F_Payment.date_sk AS CHAR(8)), 112)), 
-		   MONTH(CONVERT(DATE, CAST(F_Payment.date_sk AS CHAR(8)), 112)),  
-		   D_Subscription.plan_type;  -- Agrupa por ano, mês e tipo de plano
+		   MONTH(CONVERT(DATE, CAST(F_Payment.date_sk AS CHAR(8)), 112)),
+		   DAY(CONVERT(DATE, CAST(F_Payment.date_sk AS CHAR(8)), 112)),  
+		   D_Subscription.plan_type;  -- Agrupa por ano, mês, dia e tipo de plano
 
 
 -- VIEW 2: Evolução de Clientes (Novos Clientes por Mês)
@@ -23,10 +24,11 @@ GROUP BY   YEAR(CONVERT(DATE, CAST(F_Payment.date_sk AS CHAR(8)), 112)),
 CREATE VIEW vw_Evolucao_Clientes AS
 SELECT
     YEAR(D_Subscription.start_date) AS Ano,                   -- Ano de início da assinatura
-    MONTH(D_Subscription.start_date) AS Mes,                  -- Mês de início
+    MONTH(D_Subscription.start_date) AS Mes,                  -- Mês de início da assinatura
+    DAY(D_Subscription.start_date) AS Dia,                    -- Dia de início da assinatura 
     COUNT(DISTINCT D_Subscription.subscription_sk) AS Novos_Clientes  -- Número de novos clientes no mês
 FROM D_Subscription
-GROUP BY YEAR(D_Subscription.start_date), MONTH(D_Subscription.start_date);  -- Agrupa por ano e mês
+GROUP BY YEAR(D_Subscription.start_date), MONTH(D_Subscription.start_date),DAY(D_Subscription.start_date);  -- Agrupa por ano, mês e dia.
 
 
 -- VIEW 3: Churn Mensal (Cancelamentos e Taxa de Cancelamento)
@@ -38,6 +40,9 @@ SELECT
 
     -- Mês do cancelamento
     MONTH(D_Subscription.end_date) AS Mes,
+
+	-- Dia do cancelamento
+    DAY(D_Subscription.end_date) AS Dia,
 
     -- Quantidade de cancelamentos no mês
     COUNT(DISTINCT D_Subscription.subscription_sk) AS Cancelamentos,
@@ -73,7 +78,7 @@ SELECT
 
 FROM D_Subscription
 WHERE D_Subscription.is_cancelled = 1  -- Considera apenas os cancelados
-GROUP BY YEAR(D_Subscription.end_date), MONTH(D_Subscription.end_date);  -- Agrupa por ano e mês do cancelamento
+GROUP BY YEAR(D_Subscription.end_date), MONTH(D_Subscription.end_date),DAY(D_Subscription.end_date);  -- Agrupa por ano, mês e dia do cancelamento
 
 
 -- VIEW 4: Engajamento Mensal por Cliente (Atividades por Mês)
@@ -84,9 +89,10 @@ SELECT
 	D_Activity.device_type,								   -- Tipo de Dispositivo
     YEAR(D_Activity.activity_time) AS Ano,                 -- Ano da atividade
     MONTH(D_Activity.activity_time) AS Mes,                -- Mês da atividade
+    DAY(D_Activity.activity_time) AS Dia,                  -- Dia da atividade
     COUNT(*) AS Total_Atividades                           -- Total de atividades feitas no mês
 FROM D_Activity
-GROUP BY D_Activity.activity_type,D_Activity.device_type, YEAR(D_Activity.activity_time), MONTH(D_Activity.activity_time);  -- Agrupa por cliente, ano e mês
+GROUP BY D_Activity.activity_type,D_Activity.device_type, YEAR(D_Activity.activity_time), MONTH(D_Activity.activity_time),DAY(D_Activity.activity_time);  -- Agrupa por cliente, ano, mês e dia.
 
 
 -- Visualização das Views
